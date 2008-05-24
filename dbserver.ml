6cd003bac2abac7494faa77f958704c0dcc2daf9
Yaron M. Minsky <yminsky@gmail.com>
2008-05-23 21:12:43 -0400

    fixed bug in handling of send_mailsyncs flag

--- dbserver.ml
+++ dbserver.ml
@@ -631,24 +631,24 @@ struct
 	 then (Ehandlers.repeat_forever_simple checkpoint_interval checkpoint)
 	 else (Ehandlers.repeat_forever_simple sync_interval sync)) 
 	@
-	Ehandlers.repeat_forever_simple !Settings.membership_reload_time
-	 Membership.reset_membership_time
+	  Ehandlers.repeat_forever_simple !Settings.membership_reload_time
+	  Membership.reset_membership_time
 	@
-	(Ehandlers.repeat_forever 10. 
-	   (Eventloop.make_tc ~cb:transmit_keys ~timeout:0
-	      ~name:"mail transmit keys" )
-	)
+	  (if !Settings.send_mailsyncs then
+	     (Ehandlers.repeat_forever 10. 
+	        (Eventloop.make_tc ~cb:transmit_keys ~timeout:0
+	           ~name:"mail transmit keys" )
+	     )
+	   else [])
 	@
-	(if !Settings.send_mailsyncs then
-	   (Ehandlers.repeat_forever 10. 
-	      (Eventloop.make_tc ~name:"mailsync" ~timeout:0
-		 ~cb:(Mailsync.load_mailed_keys 
-			~addkey:(Keydb.add_key_merge ~newkey:false)))
-	   )
-	 else [])
+	  (Ehandlers.repeat_forever 10. 
+	     (Eventloop.make_tc ~name:"mailsync" ~timeout:0
+		~cb:(Mailsync.load_mailed_keys 
+		       ~addkey:(Keydb.add_key_merge ~newkey:false)))
+	  )
 	@
-	(Ehandlers.repeat_at_hour !Settings.stat_calc_hour
-	   calculate_stats_page)
+	  (Ehandlers.repeat_at_hour !Settings.stat_calc_hour
+	     calculate_stats_page)
       )
 
       (
