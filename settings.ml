b5c8ed8f957aba57d9fdf3b175565917088e0915
John P. Clizbe <John.Clizbe@gmail.com>
2012-05-20 23:51:17 -0500

    Copyright updating r-z main directory

--- settings.ml
+++ settings.ml
@@ -1,28 +1,31 @@
 (************************************************************************)
-(* This file is part of SKS.  SKS is free software; you can
-   redistribute it and/or modify it under the terms of the GNU General
-   Public License as published by the Free Software Foundation; either
-   version 2 of the License, or (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-   USA *)
-(***********************************************************************)
+(* settings.ml - Various and sundry settings with their defaults, plus  *)
+(*              functions for assigning new values.  This is used by the*)
+(*              getopt routines to set preferences                      *)
+(*                                                                      *)
+(* Copyright (C) 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,  *)
+(*               2011, 2012  Yaron Minsky and Contributors              *)
+(*                                                                      *)
+(* This file is part of SKS.  SKS is free software; you can             *)
+(* redistribute it and/or modify it under the terms of the GNU General  *)
+(* Public License as published by the Free Software Foundation; either  *)
+(* version 2 of the License, or (at your option) any later version.     *)
+(*                                                                      *)
+(* This program is distributed in the hope that it will be useful, but  *)
+(* WITHOUT ANY WARRANTY; without even the implied warranty of           *)
+(* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    *)
+(* General Public License for more details.                             *)
+(*                                                                      *)
+(* You should have received a copy of the GNU General Public License    *)
+(* along with this program; if not, write to the Free Software          *)
+(* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  *)
+(* USA or see <http://www.gnu.org/licenses/>.                           *)
+(************************************************************************)
 
 open StdLabels
 open MoreLabels
 module Unix=UnixLabels
 open Printf
-
-(** Various and sundry settings with their defaults, 
-  plus functions for assigning new values.  This is used by the 
-  getopt routines to set preferences *)
 
 let n = ref 0
 let set_n value = n := value
