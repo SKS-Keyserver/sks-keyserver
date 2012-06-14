61f9431cd4be0850e2ff8929f0d018ac56c9a709
John P. Clizbe <John.Clizbe@gmail.com>
2012-06-14 12:59:12 -0500

    cleanup misc annoyances in bdb

--- bdb/bdb_stubs.c
+++ bdb/bdb_stubs.c
@@ -1,25 +1,24 @@
-/************************************************************************)
-(* bdb_stubs.c - Stubs appropriate for Berkeley DB 4.x and DB 5.x       *)
-(*                                                                      *)
-(* Copyright (C) 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,  *)
-(*               2011, 2012  Yaron Minsky and Contributors              *)
-(*                                                                      *)
-(* This file is part of SKS.  SKS is free software; you can             *)
-(* redistribute it and/or modify it under the terms of the GNU General  *)
-(* Public License as published by the Free Software Foundation; either  *)
-(* version 2 of the License, or (at your option) any later version.     *)
-(*                                                                      *)
-(* This program is distributed in the hope that it will be useful, but  *)
-(* WITHOUT ANY WARRANTY; without even the implied warranty of           *)
-(* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    *)
-(* General Public License for more details.                             *)
-(*                                                                      *)
-(* You should have received a copy of the GNU General Public License    *)
-(* along with this program; if not, write to the Free Software          *)
-(* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  *)
-(* USA or see <http://www.gnu.org/licenses/>.                           *)
-(************************************************************************/
-
+/***********************************************************************)
+(* bdb_stubs.c - Stubs appropriate for Berkeley DB 4.x and DB 5.x      *)
+(*                                                                     *)
+(* Copyright (C) 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, *)
+(*               2011, 2012  Yaron Minsky and Contributors             *)
+(*                                                                     *)
+(* This file is part of SKS.  SKS is free software; you can            *)
+(* redistribute it and/or modify it under the terms of the GNU General *)
+(* Public License as published by the Free Software Foundation; either *)
+(* version 2 of the License, or (at your option) any later version.    *)
+(*                                                                     *)
+(* This program is distributed in the hope that it will be useful, but *)
+(* WITHOUT ANY WARRANTY; without even the implied warranty of          *)
+(* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU   *)
+(* General Public License for more details.                            *)
+(*                                                                     *)
+(* You should have received a copy of the GNU General Public License   *)
+(* along with this program; if not, write to the Free Software         *)
+(* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 *)
+(* USA or see <http://www.gnu.org/licenses/>.                          *)
+(***********************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -216,7 +215,7 @@ value caml_db_init(value v){
 //+ type dbenv
 //+ type db
 
-void raise_db(char *msg) {
+void raise_db(const char *msg) {
   raise_with_string(*caml_db_exn, msg);
 }
 
@@ -710,7 +709,7 @@ value caml_db_get_size(value db) {
   CAMLparam1(db);
   int err;
   void *stat;
-  int size;
+  int size = 0;
   DB_TXN *txn = NULL;
 
   test_db_closed(db);
@@ -1258,7 +1257,7 @@ value caml_txn_checkpoint(value dbenv, value vkbyte, value vmin,
   err = UW_dbenv(dbenv)->txn_checkpoint(UW_dbenv(dbenv),kbyte,min,flags);
   if (err != 0) {
     //fprintf(stderr,"Error found: %d\n",err); fflush(stderr);
-    if (err = EINVAL) {
+    if (err == EINVAL) {
       invalid_argument("caml_txn_checkpoint: no reason specified");
     } else {
       UW_dbenv(dbenv)->err(UW_dbenv(dbenv), err, "caml_txn_checkpoint");
