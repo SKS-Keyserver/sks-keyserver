27a050d093105e16031a8bf17f03f7443be7c500
kristianf <kristianf@Kristian-Precis.sumptuous.sumptuouscapital.com>
2012-06-16 17:47:29 +0100

    Further MLI cleanup, Set, Zset and Map

--- add_mail.mli
+++ add_mail.mli
@@ -1,46 +1,10 @@
 module Map :
   sig
     type ('a, 'b) t = ('a, 'b) PMap.Map.t
-    val empty : ('a, 'b) t
-    val add : key:'a -> data:'b -> ('a, 'b) t -> ('a, 'b) t
-    val find : 'a -> ('a, 'b) t -> 'b
-    val remove : 'a -> ('a, 'b) t -> ('a, 'b) t
-    val mem : 'a -> ('a, 'b) t -> bool
-    val iter : f:(key:'a -> data:'b -> unit) -> ('a, 'b) t -> unit
-    val map : f:('a -> 'b) -> ('c, 'a) t -> ('c, 'b) t
-    val mapi : f:(key:'a -> data:'b -> 'c) -> ('a, 'b) t -> ('a, 'c) t
-    val fold :
-      f:(key:'a -> data:'b -> 'c -> 'c) -> ('a, 'b) t -> init:'c -> 'c
-    val of_alist : ('a * 'b) list -> ('a, 'b) t
-    val to_alist : ('a, 'b) t -> ('a * 'b) list
   end
 module Set :
   sig
     type 'a t = 'a PSet.Set.t
-    val empty : 'a t
-    val is_empty : 'a t -> bool
-    val mem : 'a -> 'a t -> bool
-    val add : 'a -> 'a t -> 'a t
-    val singleton : 'a -> 'a t
-    val remove : 'a -> 'a t -> 'a t
-    val union : 'a t -> 'a t -> 'a t
-    val inter : 'a t -> 'a t -> 'a t
-    val diff : 'a t -> 'a t -> 'a t
-    val compare : 'a t -> 'a t -> int
-    val equal : 'a t -> 'a t -> bool
-    val subset : 'a t -> 'a t -> bool
-    val iter : f:('a -> unit) -> 'a t -> unit
-    val fold : f:('a -> 'b -> 'b) -> 'a t -> init:'b -> 'b
-    val for_all : f:('a -> bool) -> 'a t -> bool
-    val exists : f:('a -> bool) -> 'a t -> bool
-    val filter : f:('a -> bool) -> 'a t -> 'a t
-    val partition : f:('a -> bool) -> 'a t -> 'a t * 'a t
-    val cardinal : 'a t -> int
-    val elements : 'a t -> 'a list
-    val min_elt : 'a t -> 'a
-    val max_elt : 'a t -> 'a
-    val choose : 'a t -> 'a
-    val of_list : 'a list -> 'a t
   end
 val anonymous : string list ref
 val usage_string : string
