816059aca0b0653f9458eb5c8fcb12cb4c7bed1e
John P. Clizbe <John.Clizbe@gmail.com>
2012-06-16 16:37:44 -0500

    Spacing from bif .diff
    wipes out 1/3 of crufty diff stuff

--- bdb/bdb.ml
+++ bdb/bdb.ml
@@ -19,7 +19,7 @@
 (* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 *)
 (* USA or see <http://www.gnu.org/licenses/>.                          *)
 (***********************************************************************)
-
+ 
 (* Exception declarations *)
 
 exception DBError of string
