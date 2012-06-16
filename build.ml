c70ddbca8349a553e5540c6c1ef7626464d74873
Yaron Minsky <yminsky@janestreet.com>
2012-06-16 15:29:41 -0400

    more interface tweaks

--- build.ml
+++ build.ml
@@ -1,6 +1,4 @@
 (************************************************************************)
-(* build.ml -  Executable: Builds up the key database from a multi-file *)
-(*             database dump. dump files are taken from the command-line*)
 (*                                                                      *)
 (* Copyright (C) 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,  *)
 (*               2011, 2012  Yaron Minsky and Contributors              *)
@@ -21,8 +19,7 @@
 (* USA or see <http://www.gnu.org/licenses/>.                           *)
 (************************************************************************)
 
-module F(M:sig end) =
-struct
+module F(M:sig end) = struct
   open StdLabels
   open MoreLabels
   open Printf
@@ -43,72 +40,72 @@ struct
   let n = match !Settings.n with 0 -> 1 | x -> x
   let fnames = !Settings.anonlist
 
-  let rec get_keys_rec nextkey partial = match nextkey () with 
-      Some key -> 
+  let rec get_keys_rec nextkey partial = match nextkey () with
+      Some key ->
 	(try
-	   let ckey = Fixkey.canonicalize key in 
+	   let ckey = Fixkey.canonicalize key in
 	   get_keys_rec nextkey (ckey::partial)
 	 with
 	     Fixkey.Bad_key -> get_keys_rec nextkey partial
 	)
     | None -> partial
-	
+
   let get_keys nextkey = get_keys_rec nextkey []
 
-  let timestr sec = 
+  let timestr sec =
     sprintf "%.2f min" (sec /. 60.)
-      
+
   let rec nsplit n list = match n with
       0 -> ([],list)
     | n -> match list with
 	  [] -> ([],[])
-	| hd::tl -> 
+	| hd::tl ->
 	    let (beginning,ending) = nsplit (n-1) tl in
 	    (hd::beginning,ending)
 
-  let rec batch_iter ~f n list = 
+  let rec batch_iter ~f n list =
     match nsplit n list with
 	([],_) -> ()
       | (firstn,rest) -> f firstn; batch_iter ~f n rest
 
-  let get_keys_fname fname start = 
+  let get_keys_fname fname start =
     let cin = new Channel.sys_in_channel (open_in fname) in
-    protect 
-      ~f:(fun () -> 
+    protect
+      ~f:(fun () ->
 	    let nextkey = Key.next_of_channel cin in
 	    get_keys_rec nextkey start
 	 )
       ~finally:(fun () -> cin#close)
 
-  let get_keys_multi flist = 
+  let get_keys_multi flist =
     List.fold_left ~f:(fun keys fname -> get_keys_fname fname keys)
       flist ~init:[]
 
   let dbtimer = MTimer.create ()
   let timer = MTimer.create ()
-  
+
   (***************************************************************)
 
   let () = Sys.set_signal Sys.sigusr1 Sys.Signal_ignore
   let () = Sys.set_signal Sys.sigusr2 Sys.Signal_ignore
 
   (***************************************************************)
-  
-  let run () = 
+
+  let run () =
     set_logfile "build";
 
     if Sys.file_exists (Lazy.force Settings.dbdir) then (
       printf "KeyDB directory already exists.  Exiting.\n";
       exit (-1)
     );
-    Unix.mkdir (Lazy.force Settings.dbdir) 0o700; 
+    Unix.mkdir (Lazy.force Settings.dbdir) 0o700;
 
     Keydb.open_dbs settings;
     Keydb.set_meta ~key:"filters" ~data:"yminsky.dedup";
-    
-    protect 
+
+    protect
       ~f:(fun () ->
-	    batch_iter n fnames 
+	    batch_iter n fnames
 	    ~f:(fun fnames ->
 		  MTimer.start timer;
 		  printf "Loading keys..."; flush stdout;
@@ -118,9 +115,9 @@ struct
 		  Keydb.add_keys keys;
 		  MTimer.stop dbtimer;
 		  MTimer.stop timer;
-		  printf "DB time:  %s.  Total time: %s.\n" 
-		    (timestr (MTimer.read dbtimer)) 
-		    (timestr (MTimer.read timer)); 
+		  printf "DB time:  %s.  Total time: %s.\n"
+		    (timestr (MTimer.read dbtimer))
+		    (timestr (MTimer.read timer));
 		  flush stdout;
 	       )
 	 )
