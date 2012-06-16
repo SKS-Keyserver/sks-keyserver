4a0675b7ec9574e6461c76e2ad6d0d21fc42baa2
kristianf <kristianf@Kristian-Precis.sumptuous.sumptuouscapital.com>
2012-06-16 17:47:29 +0100

    Further MLI cleanup, Set, Zset and Map

--- prefixTree.mli
+++ prefixTree.mli
@@ -1,59 +1,10 @@
 module Set :
   sig
     type 'a t = 'a PSet.Set.t
-    val empty : 'a t
-    val is_empty : 'a t -> bool
-    val mem : 'a -> 'a t -> bool
-    val add : 'a -> 'a t -> 'a t
-    val singleton : 'a -> 'a t
-    val remove : 'a -> 'a t -> 'a t
-    val union : 'a t -> 'a t -> 'a t
-    val inter : 'a t -> 'a t -> 'a t
-    val diff : 'a t -> 'a t -> 'a t
-    val compare : 'a t -> 'a t -> int
-    val equal : 'a t -> 'a t -> bool
-    val subset : 'a t -> 'a t -> bool
-    val iter : f:('a -> unit) -> 'a t -> unit
-    val fold : f:('a -> 'b -> 'b) -> 'a t -> init:'b -> 'b
-    val for_all : f:('a -> bool) -> 'a t -> bool
-    val exists : f:('a -> bool) -> 'a t -> bool
-    val filter : f:('a -> bool) -> 'a t -> 'a t
-    val partition : f:('a -> bool) -> 'a t -> 'a t * 'a t
-    val cardinal : 'a t -> int
-    val elements : 'a t -> 'a list
-    val min_elt : 'a t -> 'a
-    val max_elt : 'a t -> 'a
-    val choose : 'a t -> 'a
-    val of_list : 'a list -> 'a t
   end
 module ZSet :
   sig
-    type elt = ZZp.zz
     type t = ZZp.Set.t
-    val empty : t
-    val is_empty : t -> bool
-    val mem : elt -> t -> bool
-    val add : elt -> t -> t
-    val singleton : elt -> t
-    val remove : elt -> t -> t
-    val union : t -> t -> t
-    val inter : t -> t -> t
-    val diff : t -> t -> t
-    val compare : t -> t -> int
-    val equal : t -> t -> bool
-    val subset : t -> t -> bool
-    val iter : f:(elt -> unit) -> t -> unit
-    val fold : f:(elt -> 'a -> 'a) -> t -> init:'a -> 'a
-    val for_all : f:(elt -> bool) -> t -> bool
-    val exists : f:(elt -> bool) -> t -> bool
-    val filter : f:(elt -> bool) -> t -> t
-    val partition : f:(elt -> bool) -> t -> t * t
-    val cardinal : t -> int
-    val elements : t -> elt list
-    val min_elt : t -> elt
-    val max_elt : t -> elt
-    val choose : t -> elt
-    val split : elt -> t -> t * bool * t
   end
 exception Bug of string
 type key = Bitstring.t
