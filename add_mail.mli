214fca8ef30f0eb35daf895636b648f9245ce397
Yaron Minsky <yminsky@janestreet.com>
2012-06-16 15:24:33 -0400

    zeroed out mlis of exe's

--- add_mail.mli
+++ add_mail.mli
@@ -1,7 +1,1 @@
-val anonymous : string list ref
-val usage_string : string
-val anon_options : string -> unit
-val parse_spec : 'a list
-val dirname : string
-val pipe_file : in_channel -> out_channel -> unit
-val run : unit -> unit
+(* This is the sks add_mail command-line tool *)
--- sks.mli
+++ sks.mli
@@ -1,11 +1,1 @@
-type command = {
-  name : string;
-  usage : string;
-  desc : string;
-  func : unit -> unit;
-}
-val usage : command -> string
-val space : Str.regexp
-val commands : command list
-val help : unit -> unit
-val find : string -> command list -> command
+(* This is the base sks command *)
