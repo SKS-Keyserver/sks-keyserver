c2aa545fd45ec8e50d74643f8a69f171a7b73cfc
kristianf <kristianf@Kristian-Precis.sumptuous.sumptuouscapital.com>
2012-07-02 05:21:45 +0100

    Incorporating comments from Yaron

--- version.ml
+++ version.ml
@@ -21,23 +21,35 @@
 (***********************************************************************)
 
 open Printf
-open Bdb
 
+let bdb_version = Bdb.version ()
+
+let sopen dirname flags mode = 
+    let dbenv = Bdb.Dbenv.create (); in
+    Bdb.Dbenv.dopen dbenv dirname flags mode; 
+    dbenv
+
+let stats = 
+	let dbenv = sopen (Lazy.force Settings.dbdir) [Bdb.Dbenv.CREATE] 0o400 in
+	let s = Bdb.Dbenv.get_dbenv_stats(dbenv) in 
+	s
+
+let dbstats_dir = 
+   let split = Str.regexp_string "." in
+   let s = 
+     match Str.split split bdb_version with
+       | major::minor::_ -> sprintf "Further details can be seen by executing \
+         db%s.%s_stat -x in the KDB and Ptree directories\n" major minor;
+       | [] | _::[] -> sprintf "Further details can be seen by executing \
+         db%s.%s_stat -x in the KDB and Ptree directories\n" "X" "Y" in
+	s
+	 
 let run() =  
-   printf "SKS version %s%s\nThis version has a minimum compatibility requirement for recon of SKS %s\n" Common.version Common.version_suffix Common.compatible_version_string; 
-   let bdb_version = version() in 
-   printf "Compiled with BDB version %s\n" bdb_version;
+   printf "SKS version %s%s\nThis version has a minimum compatibility \
+   requirement for recon of SKS %s\n" Common.version
+   Common.version_suffix Common.compatible_version_string;
    
-   let sopen dirname flags mode = 
-     let dbenv = Dbenv.create () in
-     Dbenv.dopen dbenv dirname flags mode;
-     dbenv
-   in
-   let dbenv = sopen (Lazy.force Settings.dbdir) [Dbenv.CREATE] 0o400 in
-   let stats = Dbenv.get_dbenv_stats(dbenv);  in 
-     printf "Detailed BDB environment statistics:\n%s\n" stats;
-
-   let bdb_version = version() in
-   match (Str.split (Str.regexp_string ".") bdb_version) with
-   | major::minor::_ -> printf "Further details can be seen by executing db%s.%s_stat -x in the KDB and Ptree directories\n" major minor;
-   | [] | _::[] -> printf "Further details can be seen by executing db%s.%s_stat -x in the KDB and Ptree directories\n" "X" "Y";
+   printf "Compiled with BDB version %s\n" bdb_version;
+   printf "Detailed BDB environment statistics:\n%s" stats;
+   printf "%s" dbstats_dir; 
+   
